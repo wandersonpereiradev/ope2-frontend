a0d6789501105aeb9917a4b60bea359492b706ff
AlexCastino <alexander.feitosa@aluno.faculdadeimpacta.com.br>
2021-10-05 22:36:20 -0300

    Nova tabela de produtos

--- src/pages/produtos/index.js
+++ src/pages/produtos/index.js
@@ -1,41 +1,153 @@
 import React, { Component } from "react";
 import api from '../API/api';
 import '../Estilo/estilo.css'
-class ListarProdutos extends Component{
-  state={
-    produtos:[],
+import '../Estilo/menu.css'
+import '../Estilo/styles.css';
+class ListarProdutos extends Component {
+  state = {
+    produtos: [],
   }
 
-  async componentDidMount(){
+  async componentDidMount() {
     const response = await api.get('produtos/?format=json');
     console.log(response.data);
-    this.setState({produtos: response.data})    
+    this.setState({ produtos: response.data })
+    
   }
-  render(){
-  const {produtos}= this.state;
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////d
+
+
+
+
+  render() {
+    const { produtos } = this.state;
+
+    const ClassificarDados = (produtosnovo, config = null) => {
+        const [ConfigOrdenacao, definirConfigOrdenacao] = React.useState(config);
+      
+        const ItensOrdenados = React.useMemo(() => {
+          let ListaItensOrdenar = [...produtosnovo];
+          console.log(produtosnovo)
+          if (ConfigOrdenacao !== null) {
+            
+            ListaItensOrdenar.sort((a, b) => {
+              //Exibir parametros A e B
+              console.log(a[ConfigOrdenacao.OrdenarColuna],'<<->>',b[ConfigOrdenacao.OrdenarColuna])
+
+              if (a[ConfigOrdenacao.OrdenarColuna] < b[ConfigOrdenacao.OrdenarColuna]) {
+                return ConfigOrdenacao.Direcao === 'ascendente' ? -1 : 1;
+              }
+              if (a[ConfigOrdenacao.OrdenarColuna] > b[ConfigOrdenacao.OrdenarColuna]) {
+                return ConfigOrdenacao.Direcao === 'ascendente' ? 1 : -1;
+              }
+              return 0;
+            });
+          }
+          return ListaItensOrdenar;
+        }, [produtosnovo, ConfigOrdenacao]);
+
+        const Ordenar = (OrdenarColuna) => {
+            let Direcao = 'ascendente';
+            if (
+              ConfigOrdenacao &&
+              ConfigOrdenacao.OrdenarColuna === OrdenarColuna &&
+              console.log(ConfigOrdenacao.OrdenarColuna) &&
+              ConfigOrdenacao.Direcao === 'ascendente'
+            ) {
+              Direcao = 'descendente';
+            }
+            definirConfigOrdenacao({ OrdenarColuna, Direcao });
+          };
+        
+          return { produtosnovo: ItensOrdenados, Ordenar, ConfigOrdenacao };
+        };
+
+        const TabelaProdutos = (props) => {
+            const { produtosnovo, Ordenar, ConfigOrdenacao } = ClassificarDados(props.products);
+            const ObterClasse = (nome) => {
+              if (!ConfigOrdenacao) {
+                return;
+              }
+              return ConfigOrdenacao.OrdenarColuna === nome ? ConfigOrdenacao.Direcao : undefined;
+            };
+            return (
+              <table>
+                <caption><h3>Lista de Produtos</h3></caption>
+                <br></br>
+                <thead>
+                  <tr>
+                  <th>
+                      <button
+                        type="button"
+                        onClick={() => Ordenar('codproduto')}
+                        className={ObterClasse('codproduto')}
+                      >
+                        Codigo do Produto
+                      </button>
+                    </th>
+                    <th>
+                      <button
+                        type="button"
+                        onClick={() => Ordenar('nome')}
+                        className={ObterClasse('nome')}
+                      >
+                        Descrição
+                      </button>
+                    </th>
+                    <th>
+                      <button
+                        type="button"
+                        onClick={() => Ordenar('categoria')}
+                        className={ObterClasse('categoria')}
+                      >
+                        Categoria
+                      </button>
+                    </th>
+                    <th>
+                      <button
+                        type="button"
+                        onClick={() => Ordenar('quantidade')}
+                        className={ObterClasse('quantidade')}
+                      >
+                        Quantidade Estoque
+                      </button>
+                    </th>
+                    <th>
+                      <button
+                        type="button"
+                        onClick={() => Ordenar('preco')}
+                        className={ObterClasse('preco')}
+                      >
+                        Preço
+                      </button>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {produtosnovo.map((item) => (
+                    <tr OrdenarColuna={item.id_produto}>
+                      <td OrdenarColuna={item.codigo_produto}>{item.codigo_produto}</td>
+                      <td OrdenarColuna={item.descricao}>{item.descricao}</td>
+                      <td OrdenarColuna={item.categoria}>{item.categoria}</td>
+                      <td OrdenarColuna={item.quantidade}>{item.quantidade}</td>
+                      <td OrdenarColuna={item.preco_unitario}>R${item.preco_unitario}</td>
+                      
+                      
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            );
+          };
 
     return (
-      <div>
-        <h1>Listar Produtos</h1>
-        {console.log(produtos)}
-        {produtos.map(produto =>(
-          <table>
-            <tr class='tabela' key={produto.id_produto}>
-            <td class='tabela coluna'>categoria: </td>
-            <td class='tabela coluna'>{produto.categoria}</td>
-            <td class='tabela coluna'>Nome: </td>
-            <td class='tabela coluna'>{produto.descricao}</td>
-            <td class='tabela coluna'>Codigo do Produto: </td>
-            <td class='tabela coluna'>{produto.codigo_produto}</td>
-            <td class='tabela coluna'>Preço Unitario: </td>
-            <td class='tabela coluna'>{produto.preco_unitario}</td>
-            <td class='tabela coluna'>Quantidade em Estoque:</td>
-            <td class='tabela coluna'>{produto.quantidade}</td>
-            </tr>
-          </table>
-          
-        )
-        )}
+      <div class="pai">
+          <TabelaProdutos
+        products={produtos}
+      />
+    
       </div>
     )
   }
