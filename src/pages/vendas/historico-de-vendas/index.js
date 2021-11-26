d4a16ef5a3c35b80338ced89992c86a03fba3364
AlexCastino <alexander.feitosa@aluno.faculdadeimpacta.com.br>
2021-11-25 23:34:41 -0300

    'Historico de vendas'

--- src/pages/vendas/historico-de-vendas/index.js
+++ src/pages/vendas/historico-de-vendas/index.js
@@ -5,27 +5,51 @@ import '../../Estilo/estilo.css'
 import '../../Estilo/styles.css';
 
 class HistoricoVendas extends Component {
-  state = {
-    hvendas: [],
-  }
+    state = {
+        hvendas: [],
+        soma: [],
+    }
 
-  async componentDidMount() {
-    //const response = await api.get('produtos/?format=json');
-    const response = await api.get('http://127.0.0.1:8080/produtos/');
-    console.log(response.data);
-    this.setState({ hvendas: response.data })
+    async componentDidMount() {
+        //const response = await api.get('produtos/?format=json');
+        const response = await api.get('http://127.0.0.1:8080/produtos/');
+        console.log(response.data);
+        this.setState({ hvendas: response.data })
+        this.setState({ soma: response.data })
 
-  }
+    }
 
-  render() {
-    const { hvendas } = this.state;
+    render() {
+        const { hvendas } = this.state;
+        const { soma } = this.state;
+        var somavalor = 0
+        var somaitens = 0
 
-    return (
-      <section className="">
-        <TabelaDeProdutos hvendas={hvendas} />
-      </section>
-    )
-  }
+        return (
+            <section className="">
+                <div class="ocultar">
+                    {soma.map((produto) => (
+                        somavalor = somavalor + parseInt(produto.preco_unitario),
+                        somaitens = somaitens + parseInt(produto.quantidade)
+
+                    ))}{console.log("somou", somavalor)}
+                </div>
+                <TabelaDeProdutos hvendas={hvendas} soma={somavalor}/>
+                <div class="divitens">
+                <div class="divitem">
+                <p class="somavendas">Total em Vendas</p>
+                <h1><a class="somavendas">R${somavalor}</a></h1>
+                </div>
+                <div class="divitem">
+                <p class="somavendas">Total de Itens  .</p>
+                <h1><a class="somavendas">{somaitens}</a></h1>
+                </div>
+                </div>
+                
+
+            </section>
+        )
+    }
 
 }
 export default HistoricoVendas;
