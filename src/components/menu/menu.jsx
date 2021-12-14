deeca5537a0caa272e8cdceca72e3b90be4e0d53
AlexCastino <alexander.feitosa@aluno.faculdadeimpacta.com.br>
2021-12-13 21:47:26 -0300

    'Menu'

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -10,16 +10,16 @@ function Menu() {
                     <hr class="hr" />
                 </ul>
                 <ul>
-                <li><a class="fonteli" href="/cliente-cadastrar"><i class="bi bi-bar-chart-line"></i> Dashboard</a></li>
+                {/* <li><a class="fonteli" href="/cliente-cadastrar"><i class="bi bi-bar-chart-line"></i> Dashboard</a></li> */}
                     <li><a class="fonteli" href="/cliente-cadastrar"><i class="bi bi-file-person"></i> Clientes</a></li>
                     <li><a class="fonteli" href="/menu-produtos"><i class="bi bi-calendar-plus"></i> Produtos</a></li>
                     <li><a class="fonteli" href="/listar-servicos"><i class="bi bi-calendar-plus"></i> Servicos</a></li>
                     <div class="dropdown">
-                    <a class="fonteli" href="/cadastro-vendas"><i class="bi bi-cart-check"></i> Vendas</a>
+                    <a class="fonteli"><i class="bi bi-cart-check"></i> Vendas</a>
                     <li class="vendas"><a class="fonteli vendas" href="/cadastro-vendas"><i class="bi bi-cart-check"></i> Cadastrar Venda</a></li>
                     <li class="vendas"><a class="fonteli vendas" href="/historico-vendas"><i class="bi bi-cart-check"></i> Historico Vendas</a></li>
                     </div>
-                    <li><a class="fonteli" href="/cadastro-compras"><i class="bi bi-cart-check"></i> Compras</a></li>
+                    {/* <li><a class="fonteli" href="/cadastro-compras"><i class="bi bi-cart-check"></i> Compras</a></li>  */}
                 </ul>
             </nav>
         </div>
